bddb2e0720c84cef95b80d4b6283a1ac603a6358
jadegrli <jade@LAPTOP-MIJOQDA7.localdomain>
2023-03-10 10:36:26 +0100

    fin code lab01

--- lab01/scrappy/crawler/spiders/wiki_spider.py
+++ lab01/scrappy/crawler/spiders/wiki_spider.py
@@ -9,6 +9,8 @@ class WikiSpider(scrapy.Spider):
     allowed_domain = "fr.wikipedia.org/wiki/"
     start_urls = ["https://fr.wikipedia.org/wiki/Britney_Spears"]
 
+
+
     # Define Elasticsearch instance
     es = Elasticsearch()
     es_index = "wikipedia"
@@ -27,16 +29,37 @@ class WikiSpider(scrapy.Spider):
             ).get()
             json_data = json.loads(schema_org)
 
-            # save the url and name in the json data in a separate dictionary
+            # count number of external links in the page
+            external_links = 0
+            for href in response.css("a::attr(href)").getall():
+                absolute_url = urljoin(response.url, href)
+                if self.allowed_domain in absolute_url:
+                    external_links += 1
+                    yield response.follow(href, self.parse)
+
+            headline = ""
+            if "headline" in json_data:
+                headline = json_data["headline"]
+
+            # parse the first paragraph of the page and remove the bold tags
+            p_tags = response.css("div.mw-parser-output > p")
+            first_paragraph_text = ""
+            if len(p_tags)!=0 :
+                for p_tag in p_tags:
+                    if p_tag.attrib.get("class") and "mw-empty-elt" in p_tag.attrib.get("class"):
+                        continue
+                    else:
+                        p_text = "".join(p_tag.css("::text").getall())
+                        first_paragraph_text += p_text
+                        break
+
+            # save the url and name in the json data in a separate dictionary and the external links count as a int
             final_dict = {
                 "url": json_data["url"],
                 "name": json_data["name"],
+                "external_links" : external_links,
+                "headline": headline,
+                "first_paragraph_text": first_paragraph_text,
             }
 
             self.es.index(index=self.es_index, body=final_dict)
-
-        # Find all links to subpages of fr.wikipedia.org
-        for href in response.css("a::attr(href)").getall():
-            absolute_url = urljoin(response.url, href)
-            if self.allowed_domain in absolute_url:
-                yield response.follow(href, self.parse)
